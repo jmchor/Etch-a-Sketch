464c475b5da040aaaeaf03d77d1fb79a66731a22
jc <jchorzempa@posteo.org>
2022-08-17 09:23:42 +0200

    Add js code for creating grid

---
+++ script.js
@@ -0,0 +1,30 @@
+const mainframe = document.querySelector('.main');
+
+function createGrid(num) {
+	for (let i = 1; i <= num; i++) {
+		newRow = document.createElement('div');
+		newRow.classList.add('row');
+		mainframe.appendChild(newRow);
+
+		for (let i = 1; i <= num; i++) {
+			newSquare = document.createElement('div');
+			newSquare.classList.add('square');
+			newRow.appendChild(newSquare);
+		}
+	}
+}
+function startButton() {
+	let num = parseInt(prompt('Please put in a number!'));
+
+	if (mainframe.hasChildNodes()) {
+		while (mainframe.firstChild) {
+			mainframe.removeChild(mainframe.firstChild);
+		}
+		createGrid(num);
+	} else {
+		createGrid(num);
+	}
+}
+
+const btn = document.querySelector('#gridCreator');
+btn.addEventListener('click', startButton);
